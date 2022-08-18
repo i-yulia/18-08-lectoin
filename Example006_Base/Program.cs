5b1ff176ed4ea351e3d00fca0018c97ff0e0da28
yulia <kimkaaa@mail.ru>
2022-08-18 19:47:36 +0300

    Example006_Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a =1;
+int b = 2;
+int с = 6;
+int d = 8;
+int e = 4;
+
+int max = a;
+
+if (a > max ) max = a;
+if (b > max) max = b;
+if (с > max ) max = с;
+if (d > max ) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
